a1229794fbfd33cb4e9d936e351539514b10539b
renantdai <renankh@hotmail.com>
2023-12-17 19:42:46 -0300

    CHORE: Ajusa campos do XML para deixar dinamicos

--- app/Services/EnvioLeituraService.php
+++ app/Services/EnvioLeituraService.php
@@ -4,10 +4,12 @@ namespace App\Services;
 
 use App\DTO\CreateCaptureDTO;
 use App\Services\SoapClientCMVService;
-use stdClass;
 
 const SOAP_URL_LEITURA = "https://cmv-ws.sefazrs.rs.gov.br/ws/cmvRecepcaoLeitura/cmvRecepcaoLeitura.asmx";
 const SOAP_ACTION_LEITURA = 'http://www.portalfiscal.inf.br/cmv/wsdl/cmvRecepcaoLeitura/CMVRecepcaoLeitura';
+
+const UF_CODIGO_IBGE = 43;
+const CNPJ_EMPRESA = '90256652000184';
 
 class EnvioLeituraService {
     public string $xmlPostString;
@@ -22,11 +24,27 @@ class EnvioLeituraService {
     }
 
     public function setXmlPostString(): void {
-        $this->xmlPostString = '<?xml version="1.0" encoding="utf-8"?><soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope"><soap12:Body><oneDadosMsg xmlns="http://www.portalfiscal.inf.br/cmv/wsdl/cmvRecepcaoLeitura"><oneRecepLeitura versao="2.00" xmlns="http://www.portalfiscal.inf.br/one"><tpAmb>1</tpAmb><verAplic>SVRS</verAplic><tpTransm>N</tpTransm><dhTransm>2023-07-14T13:24:29-03:00</dhTransm><infLeitura><cUF>43</cUF><dhPass>2023-07-14T13:24:29-03:00</dhPass><CNPJOper>90256652000184</CNPJOper><cEQP>000000000001001</cEQP><placa>ANZ9192</placa><tpVeiculo>1</tpVeiculo><foto>' . $this->dto->image . '</foto><indiceConfianca>100</indiceConfianca></infLeitura></oneRecepLeitura></oneDadosMsg></soap12:Body></soap12:Envelope>';
+        $this->xmlPostString = '<?xml version="1.0" encoding="utf-8"?><soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope"><soap12:Body><oneDadosMsg xmlns="http://www.portalfiscal.inf.br/cmv/wsdl/cmvRecepcaoLeitura"><oneRecepLeitura versao="2.00" xmlns="http://www.portalfiscal.inf.br/one">';
+        $this->xmlPostString .= '<tpAmb>1</tpAmb>';
+        $this->xmlPostString .= '<verAplic>SVRS</verAplic>';
+        $this->xmlPostString .= '<tpTransm>N</tpTransm>';
+        $this->xmlPostString .= '<dhTransm>2023-07-14T13:24:29-03:00</dhTransm>';
+
+        $this->xmlPostString .= '<infLeitura>';
+        $this->xmlPostString .= '<cUF>' . UF_CODIGO_IBGE . '</cUF>';
+        $this->xmlPostString .= '<dhPass>2023-07-14T13:24:29-03:00</dhPass>';
+        $this->xmlPostString .= '<CNPJOper>' . CNPJ_EMPRESA . '</CNPJOper>';
+        $this->xmlPostString .= '<cEQP>0000000000010' . $this->dto->idCam . '</cEQP>'; //criar regra para validar 15 digitos
+        $this->xmlPostString .= '<placa>' . $this->dto->plate . '</placa>';
+        $this->xmlPostString .= '<tpVeiculo>1</tpVeiculo>';
+        $this->xmlPostString .= '<foto>' . $this->dto->image . '</foto>';
+        $this->xmlPostString .= '<indiceConfianca>100</indiceConfianca>';
+        $this->xmlPostString .= '</infLeitura>';
+
+        $this->xmlPostString .= '</oneRecepLeitura></oneDadosMsg></soap12:Body></soap12:Envelope>';
     }
 
     public function sendRecord() {
-
         $soapClient = new SoapClientCMVService(SOAP_URL_LEITURA, SOAP_ACTION_LEITURA);
         $soapClient->setXmlPostString($this->getXmlPostString());
         $retorno = $soapClient->sendCurl();

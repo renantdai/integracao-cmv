e1c8b33c16b1a062fe7f7f40be7d25a99d768d78
renantdai <renankh@hotmail.com>
2024-05-20 12:59:17 -0300

    FEAT: Adiciona botão de envio da camera

--- resources/views/admin/cams/partials/content.blade.php
+++ resources/views/admin/cams/partials/content.blade.php
@@ -24,6 +24,10 @@
                             <th scope="col" class="relative py-3.5 px-4">
                                 <span class="sr-only">Ver</span>
                             </th>
+
+                            <th scope="col" class="relative py-3.5 px-4">
+                                <span class="sr-only">Ações</span>
+                            </th>
                         </tr>
                     </thead>
                     <tbody class="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
@@ -44,7 +48,7 @@
                             </td>
                             <td class="px-4 py-2 text-sm whitespace-nowrap">
                                 <div class="flex items-center">
-                                    <!--  -->
+                                    {{ $cam->statusSend }}
                                 </div>
                             </td>
 
@@ -60,6 +64,16 @@
                                 </a>
 
                                 <!--  -->
+                            </td>
+
+                            <td class="px-4 py-2 text-sm whitespace-nowrap">
+                                <a href="{{  route('cams.send', $cam->id) }}" class="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+
+                                    <span>Enviar</span>
+                                </a>
                             </td>
                         </tr>
                         @endforeach
